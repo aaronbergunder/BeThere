d799cf86b1cf3170e08858984408dd4a94ecf372
Aaron Bergunder <aaronbergunder@gmail.com>
2014-01-03 14:54:16 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeThere::Application.config.secret_key_base = '6353b95f6f035db3543bd012f970d3474b12c3df31371606241d10b35a894f54fc9a448d9451896831fdd9092b1ec42c4b0b6ecd55895424ba69f344be168426'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BeThere::Application.config.session_store :cookie_store, key: '_BeThere_session'
